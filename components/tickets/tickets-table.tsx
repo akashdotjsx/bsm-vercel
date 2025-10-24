33c96813bfdcd9031b779fca28a27ef88fd811f5
zufishan-kroolo <mohammed.zufishan@kroolo.com>
2025-10-24 15:25:45 +0530

    custom tickets

--- components/tickets/tickets-table.tsx
+++ components/tickets/tickets-table.tsx
@@ -724,7 +724,7 @@ function TicketRow({
       {/* Custom column cells */}
       {visibleCustomColumns.map((column: any) => (
         <td key={column.id} className="px-3 py-2.5 whitespace-nowrap border-r border-border">
-          <CustomColumnCell column={column} ticketId={ticket.dbId || ticket.id} editable={false} />
+          <CustomColumnCell column={column} ticketId={ticket.dbId || ticket.id} editable={true} />
         </td>
       ))}
 
